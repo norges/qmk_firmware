236038a8f54eeaf152bbdc9f5d0af173c7f2b710
ffabi <github@ffabi.com>
2021-10-20 13:43:00 +0200

    first working version

--- keyboards/dz60/keymaps/fabi/keymap.c
+++ keyboards/dz60/keymaps/fabi/keymap.c
@@ -67,15 +67,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt|LGUI | Space  | FN | Space|Alt|GUI|Left|Down|Right|
    * `-----------------------------------------------------------'
    */
-   [BL] = LAYOUT_60_iso_split(
+   [BL] = LAYOUT_60_iso_5x1u_split_bs_arrow(
 	//  1          2          3          4          5          6          7          8          9          10         11         12         13         14
-	    KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL     KC_BSPC, KC_DEL,
+	    KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC, KC_DEL,
         KC_TAB ,   KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,
         KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,
-        KC_LSFT,   KC_NUBS,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,   KC_UP,   MO(RL)
-        KC_LCTL,   KC_LALT,   KC_LGUI,   KC_SPC,    MO(FL),    KC_SPC,    KC_RGUI,   KC_RALT,   TO(NL),    KC_LEFT,   KC_DOWN,   KC_RIGHT
+        KC_LSFT,   KC_NUBS,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,   KC_UP,   MO(NL),
+        KC_LCTL,   KC_LALT,   KC_LGUI,   KC_SPC,    MO(FL),    KC_SPC,    KC_RGUI,   KC_RALT,   KC_LEFT,   KC_DOWN,   KC_RIGHT
         ),
-
 
   /* Keymap FL: Function Layer
    *
@@ -91,13 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |        |    |         |    |    |    |     |
    * `-----------------------------------------------------------'
    */
-   [FL] = LAYOUT_60_iso_split(
+   [FL] = LAYOUT_60_iso_5x1u_split_bs_arrow(
 	//  1          2          3          4          5          6          7          8          9          10         11         12         13         14
-	    KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_DEL,
+	    KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_DEL, KC_DEL,
         _______ ,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_PSCR,   _______,   _______,
         _______,   KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,  KC_MS_RIGHT,_______,  _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  _______,   _______,   _______,
-        _______,   _______,   _______,   _______,   _______,   KC_MS_BTN1,KC_MS_BTN2,_______,   KC_HOME,   KC_PGDOWN, KC_PGUP,   KC_END,    _______,
-        _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______),
+        _______,   _______,   _______,   _______,   _______,   KC_MS_BTN1,KC_MS_BTN2,_______,   KC_HOME,   KC_PGDOWN, KC_PGUP,   KC_END,    _______,_______,_______,
+        _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______
+        ),
 
   /* Keymap NL: Numpad Layer
    *
@@ -113,13 +113,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |        |    |         |    |    | RL |     |
    * `-----------------------------------------------------------'
    */
-   [NL] = LAYOUT_60_iso_split(
+   [NL] = LAYOUT_60_iso_5x1u_split_bs_arrow(
 	//  1          2         3          4          5          6          7          8          9        10        11        12         13         14
-	    TG(NL),    KC_P1,    KC_P2,     KC_P3,     KC_P4,     KC_P5,     KC_P6,     KC_P7,     KC_P8,   KC_P9,    KC_P0,     KC_PPLS,   KC_PMNS,   _______,
+	    TG(NL),    KC_P1,    KC_P2,     KC_P3,     KC_P4,     KC_P5,     KC_P6,     KC_P7,     KC_P8,   KC_P9,    KC_P0,     KC_PPLS,   KC_PMNS,   _______,   _______,
         _______,   KC_P4,    KC_P5,     KC_P6,     KC_PAST,   KC_PSLS,   _______,   KC_P4,     KC_P5,   KC_P6,    KC_PAST,   KC_PSLS,   _______,
         _______,   KC_P7,    KC_P8,     KC_P9,     KC_PPLS,   KC_PMNS,   _______,   KC_P1,     KC_P2,   KC_P3,    KC_PPLS,   KC_PMNS,   _______,   _______,
-        _______,   KC_P0,    KC_COMM,   KC_DOT,    KC_PEQL,   KC_PSLS,   KC_PMNS,   _______,   KC_P0,   KC_COMM,  KC_DOT,    KC_PEQL,   _______,
-        _______,   _______,  _______,   _______,   MO(FL),    _______,   _______,   _______,   TG(RL),  _______),
+        _______,   KC_P0,    KC_COMM,   KC_DOT,    KC_PEQL,   KC_PSLS,   KC_PMNS,   _______,   KC_P0,   KC_COMM,  KC_DOT,    KC_PEQL,   _______,   _______,   _______,
+        _______,   _______,  _______,   _______,   MO(FL),    _______,   _______,   _______,   TG(RL),  _______,   _______
+        ),
 
   /* Keymap RL: RGB Layer
    *
@@ -135,13 +136,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |        |    |         |    |    | BL |     |
    * `-----------------------------------------------------------'
    */
-   [RL] = LAYOUT_60_iso_split(
+   [RL] = LAYOUT_60_iso_5x1u_split_bs_arrow(
 	//  1          2          3          4          5          6          7          8          9          10         11         12         13         14
-	    TG(RL),    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   RESET,
+	    TG(RL),    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   RESET,   _______,
         _______,   RGB_TOG,   RGB_MOD,   RGB_HUI,   RGB_HUD,   _______,   _______,   RGB_SAI,   RGB_SAD,   RGB_VAI,   RGB_VAD,   _______,   _______,
         _______,   RGB_STA,   RGB_BRE,   RGB_RAI,   RGB_SWI,   _______,   _______,   RGB_SNA,   RGB_KNI,   RGB_GRA,   RGB_XMS,   _______,   _______,   _______,
-        _______,   BL_DEC,    BL_TOGG,   BL_INC,    BL_STEP,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
-        _______,   _______,   _______,   _______,   MO(FL),    _______,   _______,   _______,   TO(BL),    _______),
+        _______,   BL_DEC,    BL_TOGG,   BL_INC,    BL_STEP,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
+        _______,   _______,   _______,   _______,   MO(FL),    _______,   _______,   _______,   TO(BL),    _______,   _______
+        ),
 
 
 };
---
+++ keyboards/dz60/keymaps/fabi/rules.mk
@@ -0,0 +1,6 @@
+# Build Options
+#   comment out to disable the options.
+#
+
+CONSOLE_ENABLE = yes	# Console for debug(+400)
+COMMAND_ENABLE = yes    # Commands for debug and configuration
